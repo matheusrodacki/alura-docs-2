d3044080c804a05b34c8d2664390313d6f284a59
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-26 06:49:48 -0300

    feat: Add logout functionality and cookie management
    - Added the ability to logout by removing the "tokenJWT" cookie and redirecting to the login page.
    - Added functions to define, retrieve, and remove cookies in the new "cookies.js" file.
    - Updated the login socket handler to set the "tokenJWT" cookie upon successful authentication.
    
    This commit enhances the functionality of the application by allowing users to log out and adds cookie management to handle authentication tokens.

--- public/index.js
+++ public/index.js
@@ -1,8 +1,17 @@
 import { emitirAdicionarDocumento } from "./socket-front-index.js";
+import { obterCookie, removerCookie } from "./utils/cookies.js";
+
+const tokenJWT = obterCookie("tokenJWT");
 
 const listaDocumentos = document.getElementById("lista-documentos");
 const form = document.getElementById("form-adiciona-documento");
 const inputDocumento = document.getElementById("input-documento");
+const logoutBtn = document.getElementById("botao-logout");
+
+logoutBtn.addEventListener("click", () => {
+  removerCookie("tokenJWT");
+  window.location.href = "/login/index.html";
+});
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
--- public/login/socket-front-login.js
+++ public/login/socket-front-login.js
@@ -1,10 +1,13 @@
+import { definirCookie } from "../utils/cookies.js";
+
 const socket = io();
 
 function emitirAutenticarUsuario(dados) {
   socket.emit("autenticar_usuario", dados);
 }
 
-socket.on("autenticacao_sucesso", () => {
+socket.on("autenticacao_sucesso", (tokenJWT) => {
+  definirCookie("tokenJWT", tokenJWT);
   window.location.href = "/";
 });
 
