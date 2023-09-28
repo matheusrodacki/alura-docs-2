695990a3cc714134bd93ae72e844367a11093bb1
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-28 17:25:11 -0300

    refactor documentoController.js for better code organization
    - Created a separate function for handling the registration of documents
    - Moved the logic for socket events inside the new function, including text editing, document deletion, and user disconnection.

--- server/controllers/documentoController.js
+++ server/controllers/documentoController.js
@@ -13,31 +13,40 @@ function registrarDocumento(socket, io) {
     "selecionar_documento",
     async ({ nomeDocumento, nomeUsuario }, devolverTexto) => {
       const documento = await encontrarDocumento(nomeDocumento);
+
       if (documento) {
         socket.join(nomeDocumento);
+
         adicionarConexao({ nomeDocumento, nomeUsuario });
+
         const usuariosNoDocumento = obterUsuariosDocumento(nomeDocumento);
+
         io.to(nomeDocumento).emit("usuarios_no_documento", usuariosNoDocumento);
+
         devolverTexto(documento.texto);
       }
+
+      socket.on("texto_editor", async ({ texto, nomeDocumento }) => {
+        const atualizacao = await atualizaDocumento(nomeDocumento, texto);
+
+        if (atualizacao.modifiedCount) {
+          socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
+        }
+      });
+
+      socket.on("excluir_documento", async (nome) => {
+        const resultado = await excluirDocumento(nome);
+
+        if (resultado.deletedCount) {
+          io.emit("excluir_documento_sucesso", nome);
+        }
+      });
+
+      socket.on("disconnect", () => {
+        console.log(`Usuario ${socket.id} foi desconectado!`);
+      });
     }
   );
-
-  socket.on("texto_editor", async ({ texto, nomeDocumento }) => {
-    const atualizacao = await atualizaDocumento(nomeDocumento, texto);
-
-    if (atualizacao.modifiedCount) {
-      socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
-    }
-  });
-
-  socket.on("excluir_documento", async (nome) => {
-    const resultado = await excluirDocumento(nome);
-
-    if (resultado.deletedCount) {
-      io.emit("excluir_documento_sucesso", nome);
-    }
-  });
 }
 
 export default registrarDocumento;
